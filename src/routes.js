2f8739465692937fa290fba2e42854dc3ea75ea8
Robley Gori <robleyadrian@gmail.com>
2017-08-01 19:43:14 +0300

    [NEW] ADd file for the routes used in the project

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Home } from './App.js';
+import { Login, Register, Logout } from './components/auth.js';
+import { BucketLists, Items } from './components/bucketlists.js';
+import { BrowserRouter, Route } from 'react-router-dom';
+import history from './history';
+
+const Routes = () => {
+    return(
+        <BrowserRouter history={history}>
+            <div>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/bucketlists" component={BucketLists} />
+                <Route path="/logout" component={Logout} />
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
